49691f0c7a6502b86c8c4707f4a8e75f05510cc6
Sristi27 <sristi2705@gmail.com>
2021-10-08 16:50:49 +0530

    Quiz timer implemented

--- hello.go
+++ hello.go
@@ -5,6 +5,8 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strings"
+	"time"
 	"rsc.io/quote"
 )
 func main(){
@@ -13,6 +15,9 @@ func main(){
 	//command line flag parsing
 	filename := flag.String("csv","problems.csv","a csv file in the format of 'question,answer'")
 	//filename will be a pointer to a string
+	//timelimit
+	timeLimit := flag.Int("limit",30,"the time limit for the quiz")
+
 	flag.Parse()
 	file,err := os.Open(*filename) //actual value usedd
 	if(err!=nil){
@@ -29,22 +34,41 @@ func main(){
 		exit("Failed to parse the csv file")
 	}
 
+	//parse all the lines - ie setting up the entire csv file
 	problems:=parseLines(lines)
+
+	//after time expired send some msg to the user
+	timer:=time.NewTimer(time.Duration(*timeLimit) * time.Second) //to make same type
+	//<- timer.C  //waiting for a message from the channel - it will block the code once the message is received
+
 	correct:=0
 	for i,p:=range problems{
 		fmt.Printf("Problem #%d: %s = \n",i+1,p.q)
-		var answer string
-		fmt.Scanf("%s\n",&answer) //scanf removes all trailing spaces, it should not be used much /n->new line 
-		//answer is what user gives
-		//check if correct
-		if(answer==p.a){
-			fmt.Println("Correct")
-			correct++
-		}else{
-			fmt.Println("Wrong")
+		answerch:=make(chan string)
+		go func(){
+			var answer string
+			fmt.Scanf("%s\n",&answer) 
+			answerch<-answer //send answer to answer channel
+		}()
+		select{
+		case <- timer.C:
+			fmt.Printf("You scored %d out of %d. \n",correct,len(problems))
+			return
+		case answer:=<-answerch:
+			if(answer==p.a){
+				fmt.Println("Correct")
+				correct++
+			}else{
+				fmt.Println("Wrong")
+			}
+		// default:
+			//scanf removes all trailing spaces, it should not be used much /n->new line 
+			//answer is what user gives
+			//check if correct
+			
 		}
+		
 	}
-	
 	// fmt.Println(problems)
 	//To print score
 	fmt.Printf("You scored %d out of %d. \n",correct,len(problems))
@@ -55,8 +79,8 @@ func parseLines(lines[][] string)[]quiz{
 	for i,line:=range lines{
 		ret[i] = quiz{
 			q:line[0], //ques
-			a:line[1], //answer
-		}
+			a:strings.TrimSpace(line[1]), //answer
+		} 
 	}
 	return ret
 }
